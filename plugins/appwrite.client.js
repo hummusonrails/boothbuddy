ecc47dc766251a908ca0f3ef226480abe5e85189
Ben Greenberg <hello@yalladevrel.com>
2024-05-15 14:37:03 -0400

    provide client in app

--- plugins/appwrite.client.js
+++ plugins/appwrite.client.js
@@ -1,5 +1,5 @@
 import { defineNuxtPlugin } from '#app';
-import { Client, Account, Databases, Storage } from 'appwrite';
+import { Client, Account, Databases, Storage, Functions } from 'appwrite';
 
 export default defineNuxtPlugin((nuxtApp) => {
   const config = useRuntimeConfig();
@@ -8,7 +8,10 @@ export default defineNuxtPlugin((nuxtApp) => {
   const account = new Account(client);
   const database = new Databases(client, config.public.appwriteBoothDatabaseId);
   const storage = new Storage(client);
+  const functions = new Functions(client);
 
+  nuxtApp.provide('appwriteClient', client);
+  nuxtApp.provide('appwriteFunctions', functions);
   nuxtApp.provide('appwriteAccount', account);
   nuxtApp.provide('appwriteDatabase', database);
   nuxtApp.provide('appwriteStorage', storage);
